065d49c6f4139ace5fe55862d42327365ec17d2a
Miklós Kovács <fulcrum@kovacsmiki.com>
2023-07-10 21:29:59 +0900

    Changed start_vm script.

--- start_vm.bash
+++ start_vm.bash
@@ -2,19 +2,21 @@
 
 set -euo pipefail
 
-test ! -d tmp && mkdir tmp && chattr +C tmp
-test ! -f tmp/disk.img && qemu-img create -f raw tmp/disk.img 8G
-test ! -f tmp/OVMF_VARS.fd && cp /usr/share/edk2-ovmf/x64/OVMF_VARS.fd tmp/OVMF_VARS.fd
+declare workdir
+workdir="/tmp/fulcrum-arch"
+
+test ! -d $workdir && mkdir $workdir
+test ! -f $workdir/disk.img && qemu-img create -f raw $workdir/disk.img 16G
+test ! -f $workdir/OVMF_VARS.fd && cp /usr/share/edk2-ovmf/x64/OVMF_VARS.fd $workdir/OVMF_VARS.fd
 
 qemu-system-x86_64 \
 -accel kvm \
--smp 2 \
--m 2048M \
+-smp 4 \
+-m 4096M \
 -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
--drive if=pflash,format=raw,file=tmp/OVMF_VARS.fd,snapshot=on \
--drive file=tmp/disk.img,index=0,media=disk,snapshot=on,format=raw \
+-drive if=pflash,format=raw,file=$workdir/OVMF_VARS.fd \
+-drive file=$workdir/disk.img,index=0,media=disk,format=raw \
 -drive file=~/Downloads/archlinux-x86_64.iso,index=1,media=cdrom \
--virtfs local,path=src,mount_tag=src,security_model=mapped-xattr,readonly=on \
--display gtk,gl=off,grab-on-hover=off,show-tabs=on,show-cursor=off
+-virtfs local,path=src,mount_tag=src,security_model=mapped-xattr,readonly=on
 
 # mount the share with: mount --mkdir -t 9p src /src
