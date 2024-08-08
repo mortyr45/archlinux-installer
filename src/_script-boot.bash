dbbd5a9d6460c9faa5a73b6c970e4396138f9bab
Miklós Kovács <fulcrum@kovacsmiki.com>
2024-08-08 21:04:12 +0900

    updated script boot setup for new microcode insertion pattern

--- src/_script-boot.bash
+++ src/_script-boot.bash
@@ -8,12 +8,11 @@ set -euo pipefail
 # vars: kernel_choices
 
 # Setting up initcpios & boot scripts
-echo "HOOKS=(systemd modconf kms block keyboard sd-vconsole sd-encrypt filesystems fsck)
+echo "HOOKS=(systemd microcode modconf kms block keyboard sd-vconsole sd-encrypt filesystems fsck)
 COMPRESSION=\"zstd\"" > /mnt/etc/mkinitcpio.conf.d/custom.conf
 rm /mnt/boot/*.img
 for kernel in $(echo "$kernel_choices" | xargs); do
     echo "ALL_kver=\"/boot/vmlinuz-$kernel\"
-ALL_microcode=(/boot/*-ucode.img)
 PRESETS=('default')
 default_uki=\"/boot/efi/$kernel.efi\"" > /mnt/etc/mkinitcpio.d/$kernel.preset
 done
